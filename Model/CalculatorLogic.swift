e996f0a621853c078e9af800ce1b57b094b36f2a
elina-mns <ellie.mansurova@gmail.com>
2020-12-25 14:59:02 -0500

    perform calculation methods

--- Model/CalculatorLogic.swift
+++ Model/CalculatorLogic.swift
@@ -11,12 +11,13 @@ import UIKit
 struct CalculatorLogic {
 
     private var number: Double?
+    private var intermediateCalc: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calcFunctions(symbol: String) -> Double? {
+    mutating func calcFunctions(symbol: String) -> Double? {
         if let n = number {
             if symbol == "+/-" {
                 return n * -1
@@ -26,6 +27,29 @@ struct CalculatorLogic {
             }
             if symbol == "%" {
                 return n / 100
+            }
+            if symbol == "=" {
+                return performTwoNumberCalc(n2: n)
+            } else {
+                intermediateCalc = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumberCalc(n2: Double) -> Double? {
+        if let n1 = intermediateCalc?.n1, let operation = intermediateCalc?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation doesn't match any of the cases")
             }
         }
         return nil
